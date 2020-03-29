bac5414a3db80264c2ec56f6b3cd94d6ab3f537d
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-03-29 14:12:39 -0400

    28-427 Isolated vs Non-Isolated Tests

---
+++ src/app/shared/reverse.pipe.spec.ts
@@ -0,0 +1,9 @@
+import { ReversePipe } from "./reverse.pipe";
+
+describe('UserComponent', () => {
+
+  it('should create the app', () => {
+    let reversePipe = new ReversePipe();
+    expect(reversePipe.transform('hello')).toEqual('olleh');
+  });
+});
